9aa97bcf9f4399e60fc9d6d30b85cea5d505bc4a
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-21 17:46:23 +0300

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000 && salary > 0) {
+            System.out.println(salary - (salary * 0.15));
+        } else if (10000 < salary && salary <= 20000) {
+            System.out.println(salary - (salary * 0.18));
+        } else if (salary > 20000) {
+            System.out.println(salary - (salary * 0.20));
+        } else if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
