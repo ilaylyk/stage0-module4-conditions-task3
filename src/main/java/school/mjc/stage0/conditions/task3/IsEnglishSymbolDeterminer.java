1d847008d3cbdaf052b4ceefc35606d57d7efc05
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-21 17:46:08 +0300

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        char[] vowels = {'A', 'E', 'I', 'O', 'U', 'Y', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
+                'R', 'S', 'T', 'V', 'W', 'X', 'Z', 'r'};
+        boolean isVowel = false;
+        for (char c : vowels) {
+            if (symbol == c) {
+                isVowel = true;
+            }
+        }
+        if (isVowel) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
