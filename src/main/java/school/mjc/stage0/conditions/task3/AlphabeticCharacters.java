9ae3285078cab8f2106afbf05147bb1f469b4827
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-21 17:45:10 +0300

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y'){
+            System.out.println("Vowel");
+        } else if (character == 'B' || character == 'c' || character == 'd' || character == 'f' || character == 'g' || character == 'h'
+                || character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n' || character == 'p'
+                || character == 'q' || character == 'r' || character == 's' || character == 't' || character == 'v' || character == 'w'
+                || character == 'x' || character == 'z' ){
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
